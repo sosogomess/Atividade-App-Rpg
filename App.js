be6fc1f4b852003f4e1168e436c081ecffa3a924
sophia gomes <sophiagomes.tds2@aluno.senai.br>
2025-09-25 14:43:30 -0300

    feat: create project

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import { SafeAreaView, View, Text, TextInput, TouchableOpacity, FlasList, StyleSheet, Alert } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
+export default function App() {
+  const [characters, setCharacters] = useState([
+
+    
+  ]);
+}
